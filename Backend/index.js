a10921630d305c45f145af6c35177560b21280c6
Anas Abbas <anasabbasmalik88@gmail.com>
2024-11-21 14:59:30 +0500

    Connected with the Database

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,24 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-const app = express()
-const port = 3000;
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./utils/db.js";
+const app = express();
+dotenv.config({});
+const port = process.env.PORT || 3000;
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 const corsOptions = {
-    origin : 'http://localhost:5173',
-    credentials : true
-}
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.listen(port, ()=>{
-    console.log(`Server running at port ${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+  connectDB();
+});
