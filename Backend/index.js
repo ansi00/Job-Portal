3b0f7a42888535a356528ba1f89f011adad8c992
Anas Abbas <anasabbasmalik88@gmail.com>
2024-12-09 11:48:12 +0500

    Added Application related routes and controllers

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 const port = process.env.PORT || 3000;
 
@@ -26,6 +27,7 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
