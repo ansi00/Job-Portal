4fce42457b55dcc77d44b33fddd6f63ff764fbd9
Anas Abbas <anasabbasmalik88@gmail.com>
2024-12-10 21:47:10 +0500

    Completed Home page UI

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection.jsx";
+import CategoryCarousel from "./CategoryCarousel.jsx";
+import LatestJobs from "./LatestJobs.jsx";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
     </div>
   );
 }
