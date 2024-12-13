6494746729670f5bff58ba4344d52362b72ff632
Anas Abbas <anasabbasmalik88@gmail.com>
2024-12-13 11:19:14 +0500

    Added dummy UI for Browse page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
 import Jobs from "./components/Jobs";
-
+import Browse from "./components/Browse";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/jobs" element={<Jobs />} />
+        <Route path="/browse" element={<Browse />} />
       </Routes>
     </BrowserRouter>
   );
