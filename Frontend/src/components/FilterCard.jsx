853c133139a1594a52b4f98029aede5840e84e64
Anas Abbas <anasabbasmalik88@gmail.com>
2024-12-13 11:05:54 +0500

    Completed Jobs page dummy UI

--- Frontend/src/components/FilterCard.jsx
+++ Frontend/src/components/FilterCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Label } from "./ui/label";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 
 export default function FilterCard() {
   const filterData = [
@@ -16,9 +17,24 @@ export default function FilterCard() {
     },
   ];
   return (
-    <div>
-      <h1>Filter Jobs</h1>
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
       <hr className="mt-3" />
+      <RadioGroup>
+        {filterData.map((data, index) => (
+          <div className="font-bold text-lg">
+            {data.filterType}
+            {data.array.map((item, index) => {
+              return (
+                <div className="flex items-center space-x-2 my-2">
+                  <RadioGroupItem value={item} />
+                  <Label>{item}</Label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </RadioGroup>
     </div>
   );
 }
